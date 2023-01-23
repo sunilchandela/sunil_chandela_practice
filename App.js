237a0688a27671f3da68c5121565b90bffc3f9b3
sunilchandela <sunilchanija99@gmail.com>
2023-01-23 21:32:33 +0530

    React Class 2

--- App.js
+++ App.js
@@ -1,4 +1,13 @@
-const heading = React.createElement("h1",{},"Namaste Javascript 5");
-console.log(heading);
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
+const heading1 = React.createElement("h1",{
+    "id":"title"
+},"Heading 1 for parcel");
+const heading2 = React.createElement("h2",{
+    "id":"title"
+},"Heading 2");
+const container = React.createElement("div",{
+    "id":"container"
+},[heading1, heading2]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(container);
